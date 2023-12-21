94ffe7d90b6fb18c1a0fcb38b4aaf80459c3c937
bhavesh itankar <bhavesh.itankar@gmail.com>
2023-12-21 10:00:45 +0530

    Update datatypes.go

--- datatypes.go
+++ datatypes.go
@@ -278,6 +278,22 @@ func checkMap(key string, m1 map[string]int) bool {
 	return false
 }
 
+func solving2sum(arr []int, target int) {
+	// W.A.P , you are give an array arr = []int{1,2,3,4}, target=5, find the indexes in array which sums up to target
+	// for example arr[0]+arr[3] = 5 --> o/p [0,3], [1,2]
+	m1 := map[int]int{}
+	for idx := range arr {
+		m1[arr[idx]] = idx // m1[1]=0
+	}
+	for idx := range arr {
+		valuetobefound := target - arr[idx]
+		if v, ok := m1[valuetobefound]; ok {
+			fmt.Println("fist-index: ", idx, "second-index: ", v)
+			break
+		}
+	}
+}
+
 func main() {
 
 	// conditionalOperators()
@@ -291,8 +307,17 @@ func main() {
 	// W.A.P to, count number of times each character is repeated. ["a":4,"b":1...]
 	s1 := "abcdasaakkefg"
 	m2 := map[string]int{}
-	for _, v := range s1 {
+	/*for _, v := range s1 {
 		if checkMap(string(v), m2) {
+			// character exists
+			// m2[string(v)] = m2[string(v)] + 1
+			m2[string(v)] += 1
+		} else {
+			m2[string(v)] = 1
+		}
+	}*/
+	for _, v := range s1 {
+		if _, ok := m2[string(v)]; ok {
 			// character exists
 			// m2[string(v)] = m2[string(v)] + 1
 			m2[string(v)] += 1
@@ -303,6 +328,25 @@ func main() {
 	fmt.Println(m2)
 	// W.A.P , you are give an array arr = []int{1,2,3,4}, target=5, find the indexes in array which sums up to target
 	// for example arr[0]+arr[3] = 5 --> o/p [0,3], [1,2]
+	arr := []int{1, 2, 3, 4}
+	target := 5
+	// brutforce approch. n*n = O(n^2) --> n --> nlog(n)
+	for idx1 := range arr {
+		checkBreak := false
+		for j := idx1 + 1; j < len(arr); j++ {
+			if target == arr[idx1]+arr[j] {
+				fmt.Println("fist-index: ", idx1, "second-index: ", j)
+				checkBreak = true
+				break
+			}
+		}
+		if checkBreak {
+			break
+		}
+	}
+	// optimized solution:
+	fmt.Println("optimized solution output : ")
+	solving2sum(arr, target)
 }
 
 /*
