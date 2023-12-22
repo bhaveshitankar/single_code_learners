473c87863b7d7d18720a942ddf2d9ce3ca5c854b
bhavesh itankar <bhavesh.itankar@gmail.com>
2023-12-22 09:59:39 +0530

    Create practie.go

---
+++ practie.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	// W.A.P to find if 5 exists in the arry
+	a := []int{1, 2, 3, 4, 5, 6, 7}
+	fmt.Println(findValueInArray(5, a))
+	//findValueInArray(int,[]int) bool
+}
+
+func findValueInArray(int, []int) bool {
+    
+}
